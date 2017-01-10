859f8a534670afb90dd51ecf3da30748d24671b4
Jitse-Jan <jitsejan@gmail.com>
2017-01-10 12:00:27 +0100

    Finished tutorial radial meter for directed graph

--- appserver/static/visualizations/directed_graph/src/visualization_source.js
+++ appserver/static/visualizations/directed_graph/src/visualization_source.js
@@ -1,52 +1,28 @@
-/*
- * Visualization source
- */
 define([
             'jquery',
             'underscore',
             'api/SplunkVisualizationBase',
-            'api/SplunkVisualizationUtils'
-            // Add required assets to this list
+            'api/SplunkVisualizationUtils',
+            'd3'
         ],
         function(
             $,
             _,
             SplunkVisualizationBase,
-            vizUtils
+            SplunkVisualizationUtils,
+            d3
         ) {
   
-    // Extend from SplunkVisualizationBase
     return SplunkVisualizationBase.extend({
-  
+ 
         initialize: function() {
-            SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
+            // Save this.$el for convenience
             this.$el = $(this.el);
-
-            this.$el.append('<h3>This is a custom visualization stand in.</h3>');
-            this.$el.append('<p>Edit your custom visualization app to render something here.</p>');
-            
-            // Initialization logic goes here
+             
+            // Add a css selector class
+            this.$el.addClass('splunk-radial-meter');
         },
-
-        // Optionally implement to format data returned from search. 
-        // The returned object will be passed to updateView as 'data'
-        formatData: function(data) {
-
-            // Format data 
-
-            return data;
-        },
-  
-        // Implement updateView to render a visualization.
-        //  'data' will be the data object returned from formatData or from the search
-        //  'config' will be the configuration property object
-        updateView: function(data, config) {
-            
-            // Draw something here
-
-        },
-
-        // Search data params
+ 
         getInitialDataParams: function() {
             return ({
                 outputMode: SplunkVisualizationBase.ROW_MAJOR_OUTPUT_MODE,
@@ -54,7 +30,93 @@ define([
             });
         },
 
-        // Override to respond to re-sizing events
-        reflow: function() {}
+        formatData: function(data, config) {
+    
+            // Check for an empty data object
+            if(data.rows.length < 1){
+                return false;
+            }
+            var datum = SplunkVisualizationUtils.escapeHtml(parseFloat(data.rows[0][0]));
+        
+            // Check for invalid data
+            if(_.isNaN(datum)){
+                throw new SplunkVisualizationBase.VisualizationError(
+                    'This meter only supports numbers'
+                );
+            }
+            
+            return datum;
+        },
+  
+        updateView: function(data, config) {
+             
+        // Return if no data
+        if (!data) {
+            return;
+        }
+       
+        // Assign datum to the data object returned from formatData
+        var datum = data;
+
+        // Clear the div
+        this.$el.empty();
+ 
+        var mainColor = config[this.getPropertyNamespaceInfo().propertyNamespace + 'mainColor'] || '#f7bc38';
+    
+        var maxValue = parseFloat(config[this.getPropertyNamespaceInfo().propertyNamespace + 'maxValue']) || 100;
+
+        // Set height and width
+        var height = 220;
+        var width = 220;
+  
+        // Create a radial scale representing part of a circle
+        var scale = d3.scale.linear()
+            .domain([0, maxValue])
+            .range([ - Math.PI * .75, Math.PI * .75])
+            .clamp(true);
+  
+        // Create parameterized arc definition
+        var arc = d3.svg.arc()
+            .startAngle(function(d){
+                return scale(0);
+            })
+            .endAngle(function(d){
+                return scale(d)
+            })
+            .innerRadius(70)
+            .outerRadius(85);
+
+        // SVG setup
+        var svg  = d3.select(this.el).append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .style('background', 'white')
+            .append('g')
+            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+        // Background arc
+        svg.append('path')
+            .datum(maxValue)
+            .attr('d', arc)
+            .style('fill', 'lightgray');
+
+        // Fill arc
+        svg.append('path')
+            .datum(datum)
+            .attr('d', arc)
+            .style('fill', mainColor);
+
+        // Text
+        svg.append('text')
+            .datum(datum)
+            .attr('class', 'meter-center-text')
+            .style('text-anchor', 'middle')
+            .style('fill', mainColor)
+            .text(function(d){
+                return parseFloat(d);
+            })
+            .attr('transform', 'translate(' + 0 + ',' + 20 + ')');
+
+        }
     });
 });
